ab8f56bc15e5106bb0655344ed4bf13328ad4b19
Sam Gammon <sam@momentum.io>
2018-03-22 15:28:27 -0700

    New gitbook config for OCS

--- book.js
+++ book.js
@@ -1,5 +1,35 @@
 module.exports = {
-    title: 'Unprotocols RFCs',
+    title: 'OpenCannabis Specification (Raw/Draft)',
+    description: 'Open technical model and specification for legal cannabis data',
+    author: 'OpenCannabis Editor and Contributors',
     gitbook: '>=3.0.0',
-    plugins: ['coss@1.0.6']
+    plugins: [
+      'coss',
+      'code',
+      'sitemap',
+      'insert-logo',
+      'nomnoml',
+      'swagger',
+      'colors',
+      'terminal',
+      'rss',
+      'plantuml-cloud-languages',
+    ],
+    pluginsConfig: {
+      sitemap: {
+        hostname: 'https://rfc.opencannabis.info/'
+      },
+      rss: {
+        title: 'OpenCannabis Revision Feed',
+        site_url: 'https://rfc.opencannabis.info',
+        author: 'Bloombox (info+opencannabis@bloombox.io)',
+        managingEditor: 'Sam Gammon (sam@bloombox.io)',
+        categories: [
+          'cannabis',
+          'technology',
+          'data',
+          'protobuf'
+        ]
+      }
+    }
 };
